35b2bf5aa407a352c76e64e541c1382fb18dccad
Gr1nnch <70350701+Gr1nnch@users.noreply.github.com>
2024-02-12 14:17:51 +0300

    small test 1

--- sbr/4_manager.f90
+++ sbr/4_manager.f90
@@ -263,8 +263,8 @@ contains
             ! вычисление g22 и g33
             call calculate_metrics(pa, tet)
 
-            yn3 = point%Ntor*dsqrt(g33)/co 
-            xm = point%Npol*dsqrt(g22)/si
+            yn3 = point%Ntor*dsqrt(g33)/co
+            xm = 0 !point%Npol*dsqrt(g22)
 
             call disp2_iroot2(pa,xm,tet,f1,f2)
             
